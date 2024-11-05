f3a7fb8caf3038eee27f9c8908d69511205177a5
Sainul Abiheen P <sainulabidheenpsainu@gmail.com>
2024-11-05 22:52:38 +0530

    Add files via upload

---
+++ functions.dart
@@ -0,0 +1,30 @@
+//   functions
+void main(){
+  var myname=name("sainul","abidheen p");
+  print (myname);
+}
+
+// 1 = without arguments and without return types
+
+void zxc(){
+  print ("1,2,3,4,");
+}
+
+// 2= with arguments and nwithout return types
+
+void addToNumbers(int a,int b){
+  print(a+b);
+}
+
+// 3= without arguments and with return type
+
+String asd(){
+  return "sainu";
+}
+
+// 4= with arguments and with return types
+
+String name(String a,String b){
+  return a+b;
+
+}
